2a3e62843d08a3b3733c2ec43651d98a9152b6b8
mariama4 <mariama4@mail.ru>
2024-01-18 17:47:33 +0300

    update config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+data_folder = 'data'
+proxies_filename = 'proxies'
+sites_filename = 'sites'
+log_folder = 'logs'
+screenshots_folder = 'screenshots'
+proxies_folder = 'extension_proxies'
+user_data_dir = 'drivers/user_data'
+drivers_dir = 'drivers/drivers'
+
+# секунд
+chromedriver_timeout = 360
