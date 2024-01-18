7791c2b4de5f3a69dff3f0ea39440396de1fb1ec
mariama4 <mariama4@mail.ru>
2024-01-18 17:52:20 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,99 +1,113 @@
 import json
 import random
-from os import path
-from multiprocessing import Lock, Queue, Process, cpu_count
-from worker import run
-from shutil import rmtree
+import sys
+import time
+from multiprocessing import Queue, cpu_count, freeze_support
+import utils
+from config import data_folder, sites_filename, proxies_filename
+from worker import Worker
 from logger import logger
 
 
-def clear_temp_folder():
-    if path.isdir('extension_proxies'):
-        logger.info(
-            f'Очистка временной папки')
-        try:
-            rmtree(f'extension_proxies')
-        except Exception as e:
-            logger.exception(e)
-            logger.error(
-                f'Ошибка при очистки папки - extension_proxies')
-
-
 def main():
-    logger.info('Запуск приложения')
-    clear_temp_folder()
-    logger.info('Для закрытия нажмите ENTER')
+    log = logger.bind(
+        classname=__name__
+    )
+
+    log.info('Запуск приложения')
+    log.info('Для остановки приложения нажмите ENTER')
+
+    utils.delete_drivers_folder()
+    utils.delete_proxy_folder()
+    utils.create_screenshots_folder()
 
     try:
-        sites = json.load(open('data/sites.json'))['sites']
-        proxies = json.load(open('data/proxies.json'))['proxies']
-    except KeyError:
-        logger.error('Проверьте файлы sites.json/proxies.json! ' +
+        sites: list = json.load(open(f'{data_folder}/{sites_filename}.json'))['sites']
+        proxies: list = json.load(open(f'{data_folder}/{proxies_filename}.json'))['proxies']
+    except KeyError as e:
+        log.error('Проверьте файлы sites.json/proxies.json! ' +
                      'В файле sites.json наличие массива с именем "sites", ' +
                      'в файле proxies.json наличие массива с именем "proxies"!')
         return
-    except FileNotFoundError:
-        logger.error('Проверьте наличие файлов в папке data: sites.json и proxies.json!')
+    except FileNotFoundError as e:
+        log.error('Проверьте наличие файлов в папке data: sites.json и proxies.json!')
         return
     except Exception as e:
-        logger.exception(e)
-        logger.error('Неизвестная ошибка, перезапустите приложение или сообщите разработчику')
+        log.error('Неизвестная ошибка, перезапустите приложение или сообщите разработчику')
         return
 
     if len(sites) == 0:
-        logger.error('0 сайтов.')
+        log.error('0 сайтов')
         return
 
     if len(proxies) == 0:
-        logger.error('0 прокси.')
+        log.error('0 прокси')
         return
+
+    log.info(f'Количество сайтов - {len(sites)}')
+    log.info(f'Количество проксей - {len(proxies)}')
 
     random.shuffle(proxies)
 
     site_list = []
     proxy_queue = Queue()
-    lock = Lock()
 
-    logger.info(f'Количество сайтов - {len(sites)}')
     for site in sites:
         site_list.append(site)
 
-    logger.info(f'Количество прокси - {len(proxies)}')
     for proxy in proxies:
         proxy_queue.put(proxy)
 
-    num_workers = min(cpu_count() - 1, len(proxies))
+    max_num_workers = min(cpu_count() - 1, len(proxies))
 
-    logger.info(
-        'Максимальное количество воркеров ' +
-        f'(Минимальное значение из: все ядра минус 1 или количество прокси; кастомное значение) - {num_workers}')
+    while True:
+        time.sleep(0.1)
+        user_num_workers = input(
+            f'Введите количество процессов (max - {max_num_workers}):'
+        )
+        try:
+            user_num_workers = int(user_num_workers)
+        except:
+            log.warning('Некорректное значение!')
+            continue
+        if user_num_workers > max_num_workers:
+            log.warning('Введенное количество больше доступного!')
+            continue
+        break
 
-    try:
-        workers = []
-        for _ in range(num_workers):
-            worker_process = Process(target=run, args=(site_list, proxy_queue, lock))
-            logger.info(f'Запуск воркера - {worker_process.name}')
-            worker_process.start()
-            workers.append(worker_process)
-    except Exception as e:
-        logger.exception(e)
-        logger.error('Произошла ошибка при создании воркера, перезапустите приложение или сообщите разработчику.')
+    log.warning(f'Количество воркеров: {user_num_workers}')
+
+    drivers_path_list = utils.create_chromedrivers_for_workers(count=user_num_workers)
+
+    if user_num_workers == 0:
         return
 
-    if len(workers) != 0:
-        logger.warning(f'Запущено {len(workers)} воркеров')
+    workers = [Worker(name=f'worker-{idx}',
+                      site_list=site_list,
+                      proxy_queue=proxy_queue,
+                      driver_path=driver_path
+                      ) for idx, driver_path in enumerate(drivers_path_list)]
+
+    try:
+        for w in workers:
+            w.start()
+        log.warning(f'Запущено {len(workers)} воркеров')
         input()
-        for worker_process in workers:
-            worker_process.terminate()
-            worker_process.join()
-
-        clear_temp_folder()
-    else:
-        logger.warning(f'Запущено 0 воркеров.')
-
-    logger.warning(f'Завершение работы...')
-    exit(0)
+    except Exception as e:
+        log.error('Неизвестная ошибка')
+    finally:
+        log.warning(f'Завершение работы...')
+        utils.terminate_all_process(workers)
+        utils.delete_drivers_folder()
+        utils.delete_proxy_folder()
+        exit(0)
 
 
 if __name__ == '__main__':
+
+    if sys.platform.startswith('win32'):
+        from console_patch_windows import disable_quick_edit_mode
+        disable_quick_edit_mode()
+        freeze_support()
+
     main()
