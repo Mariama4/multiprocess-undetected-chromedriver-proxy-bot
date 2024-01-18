66d904405208e829fbfa1e0e0d1a68f4c506a0c9
mariama4 <mariama4@mail.ru>
2024-01-18 17:48:21 +0300

    update logger.py

--- logger.py
+++ logger.py
@@ -1,6 +1,29 @@
 from loguru import logger
+import sys
+
+logger.remove()
+
+# logger.add(sys.stdout)
 
 logger.add(
     'logs/log.log',
-    enqueue=True
+    enqueue=True,
+    format='{time:MMMM D, YYYY > HH:mm:ss} | {level} | {extra[classname]}:{extra[url]} | {message}',
+    backtrace=True,
+    diagnose=True
 )
+
+logger.add(
+    sys.stdout,
+    enqueue=True,
+    format='<m>{time:MMMM D, YYYY > HH:mm:ss}</m> | ' +
+           '<level>{level}</level> | ' +
+           '<r>{extra[classname]}</r>:<c>{extra[url]}</c> | ' +
+           '<b>{message}</b>',
+    colorize=True,
+    backtrace=True,
+    diagnose=True
+)
+
+logger.configure(extra={'classname': 'None',
+                        'url': '...'})
